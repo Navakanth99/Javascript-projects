6a4574337e6a97f64e881c27710f17d717b1359c
Navakanth <91722814+Navakanth99@users.noreply.github.com>
2024-09-25 18:00:08 +0530

    Add files via upload

---
+++ Photos-Generator/script.js
@@ -0,0 +1,17 @@
+const containerEl =document.querySelector(".container");
+const btnEl = document.querySelector(".btn");
+btnEl.addEventListener("click", ()=> {
+     imageNum=10;
+    addNewImages()
+})
+
+function addNewImages(){
+    for(let i=0; i<imageNum; i++){
+        const newImgEl =document.createElement("img")
+    newImgEl.src =`https://picsum.photos/300?random=${Math.floor(
+        Math.random() * 2000
+    )}`;
+    containerEl.appendChild(newImgEl)
+    }
+    
+}
